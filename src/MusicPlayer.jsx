de5ff99da09285ab4b90deb5e37a13c7a0d11c0f
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-05 14:13:10 +0500

    added some jsx and styles

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import img1 from "./assets/img1.jpg"
+import { FaBackward, FaForward, FaPause, FaPlay } from "react-icons/fa6";
+
 
 const MusicPlayer = () => {
+
+    const backward =() => {
+        console.log("hello");
+    }
+    const forward =() => {
+        console.log("hello");
+    }
   return (
-    <div>MusicPlayer</div>
+    <div>
+        <div>
+            <h4>Justin beaber</h4>
+            <div>
+                <img src={img1} alt="image-One" id='artist' />
+            </div>
+            <div>
+                <p>God`s plan</p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <p>0:00</p>
+                    </div>
+                    <div>
+                    <p>0:00</p>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+            <div>
+                <div onClick={backward}>
+                <FaBackward />
+                </div>
+                <div id='playing'>
+                <FaPlay />
+                <FaPause />
+                </div>
+                <div onClick={forward}>
+                    <FaForward/>
+                </div>
+                <audio src="" className=''></audio>
+            </div>
+        </div>
+    </div>
   )
 }
 
